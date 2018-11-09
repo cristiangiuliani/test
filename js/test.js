d74f6507cbe4fe1e30c048a87eb66c517b5a324a
Cristian Giuliani | Response Concepts <res.cristian@Center.THGroep.local>
2018-11-09 13:29:32 +0100

    commit

--- js/test.js
+++ js/test.js
@@ -1,3 +1,7 @@
+/*Implement the removeProperty function which takes an object and property name, and does the following:
+
+If the object obj has a property prop, the function removes the property from the object and returns true; 
+in all other cases it returns false.*/
 function removeProperty(obj, prop) {
     console.log('Start '+Object.getOwnPropertyNames(obj));
     
@@ -12,9 +16,11 @@ function removeProperty(obj, prop) {
 var myobj = {id: 1, num: 3, desc: 'testo'}
 removeProperty(myobj, 'num');
 
+
+
 function formatDate(userDate) {
     let current = new Date(userDate),
-        arrDate = [current.getFullYear(), current.getMonth(), current.getDate()],
+        arrDate = [current.getFullYear(), (current.getMonth()+1), current.getDate()],
         parseDate = arrDate.map(value => {return value < 10 ? `0${value}` : value});
     
     return parseDate.join('');
@@ -28,15 +34,14 @@ function formatDate(userDate) {
 
       buttons.forEach((item,index) => {
         item.addEventListener("click",function(){
-            console.log(this.parentElement);
-            
+            this.parentElement.remove()
         });
     });
   }
   
   window.onload = () => { 
     // Example case. 
-    document.body.innerHTML = `
+    /*document.body.innerHTML = `
     <div class="image">
         <img src="https://goo.gl/kjzfbE" alt="First">
         <button class="remove">X</button>
@@ -44,10 +49,56 @@ function formatDate(userDate) {
     <div class="image">
         <img src="https://goo.gl/d2JncW" alt="Second">
         <button class="remove">X</button>
-    </div>`;
+    </div>`;*/
     setup();
     
     //document.getElementsByClassName("remove")[0].click();
+
+    registerHandlers();
+
+    function appendChildren(decorateDivFunction) {
+        var allDivs = document.getElementsByTagName("div"),
+            count = allDivs.length;
+        
+        for (var i = 0; i < count; i++) {
+          var newDiv = document.createElement("div");
+          decorateDivFunction(newDiv);
+          allDivs[i].appendChild(newDiv);
+        }
+      }
+      
+      // Example case. 
+      document.body.innerHTML = `
+      <div id="a">
+        <div id="b">
+        </div>
+      </div>`;
+      
+      appendChildren(function(div) {});
   }
 
- 
+  function registerHandlers() {
+    var as = document.getElementsByTagName('a');
+    for (let i = 0; i < as.length; i++) {
+        console.log(i);
+      as[i].onclick = function() {
+        alert(i);
+        
+        
+        return false;
+      }
+    }
+  }
+
+
+  /*Implement the ensure function so that it throws an error if called without arguments or the argument is undefined. 
+  Otherwise it should return the given value.*/
+  function ensure(value=undefined) {
+      if(value===undefined){
+        throw "Errore";
+      }else{
+         return value;
+      }
+  }
+
+  ensure(2);
