7573bcd58e65f350013125e91df6085cc212d0d2
Cristian Giuliani | Response Concepts <res.cristian@Center.THGroep.local>
2018-11-13 16:55:26 +0100

    update

--- js/test.js
+++ js/test.js
@@ -1,3 +1,18 @@
+// Esempio di closure
+function createCounter() {
+  let counter = 0
+  const myFunction = function() {
+    counter = counter + 1
+    return counter
+  }
+  return myFunction
+}
+const increment = createCounter()
+const c1 = increment()
+const c2 = increment()
+const c3 = increment()
+console.log('example increment', c1, c2, c3)
+
 /*Implement the removeProperty function which takes an object and property name, and does the following:
 
 If the object obj has a property prop, the function removes the property from the object and returns true; 
@@ -134,6 +149,8 @@ function formatDate(userDate) {
     [].forEach.call(collection, (item,index) => { ... });
 
   - Le funzioni sono oggetti. Queste hanno le proprietá name e length. Name restituisce il nome della funzone, mentre length il numro di argomenti
+
+  - for(indice in array){ totale = totale + array[i] } -- for(valore of array){ totale = totale + valore}
   
   ---- DOM ----
     - Attributi e proprietá non sono esattamente la stessa cosa es:
@@ -190,5 +207,48 @@ function formatDate(userDate) {
     - mutation observer sono eventi che scattano quando avviene qualche variazione nel DOM
 
 
+  ---- OBJECTS ----
+    - il passaggio di valori relativi a tipi di dato primitivi avviene sempre per valore 
+      mentre il passaggio di oggetti avviene sempre per riferimento
 
+    - Gli oggetti possono essere costituiti da tipi primitivi o da strutture piú complesse
+        var numero      = new Object(12);
+        var altroNumero = new Object(3*2);
+        var stringa     = new Object("test");
+        var persona     = new Object({nome: "Mario", cognome: "Rossi"});
+      Essi hanno in comune alcuni metodi come toString() e valueOf()
+
+    - Una funzione é in realtá un oggetto first class con oggetto base function
+
+    - per cancellare una proprietá di un'oggetto si usa delete oggetto.proprieta
+
+    - Il costruttore si crea con function Oggetto() le successive istanze con new var oggetto1 = new Oggetto(); 
+
+    - Agendo sul prototipo si cambia la struttura del costruttore e di tutte le istanze Oggetto.prototype.nuovaProp = 'xxx' 
+      Questo concetto sfrutta l'ereditarietá e le istanze attingono alla nuova proprietá del costruttore come se fosse loro
+
+    - Il prototipo di un oggetto può a sua volta avere un altro prototipo. In questo caso la ricerca di una proprietà o di un metodo risale la catena dei prototipi fino ad arrivare all’oggetto Object, il prototipo base di tutti gli oggetti.
+
+    - Possiamo aggiungere proprietá e metodi ai prototipi predefiniti. Ad esempio possiamo aggiungere un nuovo metodo al prototipo string String.prototype.padLeft = function(w, c){...}
+      Ora il nuovo metodo si puó utilizzare su tutte le stringhe "xxx".padLeft(10, 'x')
+
+    - Con Object.create() é possibile creare dei descrittori> data descriptor (proprietá predefinite come writable, configurable) e accessor description (funzioni getter e setter)
+
+    - Reflection, é possibile analizzare la struttura e dati degli oggetti runtime. Esiste un oggetto Reflect
+
+    - Proxy, la classe Proxy consente di creare oggetti che hanno la capacità di modificare il comportamento predefinito di altri oggetti.
+
+    - Symbol() genera un valore univoco ma non accessibile
+
+    - Si possono serializzare gli oggetti in JSON  con JSON.stringify() e deodificarli con JSON.parse()
+
+    - PRINCIPI:
+        - Encapsulation: Si, ma le propietá e metodi privati sono definiti come semplici variabili nel costruttore
+        - Inheritance: Si, ma con modello diverso prima dell'introduzione delle classi
+        - Polimorphism: Si, risulta implicito
+        - Abstraction
+    
+    - Eccezioni: si gestiscono con try{ codice da provare }catch(e){ in caso di errore e.message e.name }finally{ da lanciare anche in caso di eccezioni}
+      si possono generare eccezioni con throw
     */
+
