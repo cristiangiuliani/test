ca553b797650426f0790e5c12c4b9066e8918b2e
Cristian Giuliani <cristiangiuliani@Cristians-MacBook-Pro.local>
2018-11-18 23:21:24 +0100

    update favorites

--- formula/js/main.js
+++ formula/js/main.js
@@ -11,7 +11,7 @@ function buildSeasonsList(){
             html += `<li data-season="${item}">${item}</li>`;
         });
         seasonsListRoot.innerHTML = html;
-        seasonsListRoot.addEventListener('click', ()=>eventWrapper(event,()=>getSeason(event)), false);
+        seasonsListRoot.addEventListener('click', ()=>eventWrapper(event,()=>getSeason(event,null, renderSeason)), false);
     }else{
 
     }
@@ -42,74 +42,53 @@ function httpHandler(type, url,callback){
     Http.send();
 }
 
-function getSeason(event){
-    let season = event.target.getAttribute('data-season'),
-        url = `http://ergast.com/api/f1/${season}/results/1.json`;
+function getSeason(event, season=null,callback){
+    if(season===null) season = event.target.getAttribute('data-season');
+    let url = `http://ergast.com/api/f1/${season}/results/1.json`;
     
     if(CACHE.seasons[season] === undefined){
         httpHandler("GET",url,Data=>{
             let races = Data.MRData.RaceTable.Races;
-            renderSeason(races);
+            callback(races);
             CACHE.seasons[season] = races;
             console.log('HTTP');
         });
     }else{
-        renderSeason(CACHE.seasons[season]);
+        callback(CACHE.seasons[season]);
         console.log('CACHE');
     }
-    console.log(CACHE);
 }
 
 function renderSeason(races){
     let seasonRaces = document.getElementById('seasonRaces'),
-        html = "<table>",
-        favorites = localStorage.getObj("favorites"),
-        button = ()=>{
-            (favorites)
-        };
+        seasonId = "S"+races[0].season;
+        html = `<table id="${seasonId}">`,
+        favorites = localStorage.getObj("favorites");
             
     races.forEach(race=>{
-        html += `
-                <tr>
-                    <td>${race.round}</td>
-                    <td>${race.raceName}</td>
-                    <td>${race.date}</td>
-                    <td>${race.Circuit.circuitName}</td>
-                    <td>${race.Circuit.Location.locality}, ${race.Circuit.Location.country}</td>
-                    <td>${race.Results[0].Driver.givenName} ${race.Results[0].Driver.familyName}</td>
-                    <td>${race.Results[0].Constructor.name}</td>
-                    <td><button class="add-fav" data-season="${race.season}" data-race="${race.round}">+</button></td>
-                </tr>`;
+        html += seasonListTemplate(race);
     });
     html += "</table>";
     seasonRaces.innerHTML = html;
-    seasonRaces.addEventListener('click', ()=>eventWrapper(event,()=>toggleFavorites(event)), false);
+    document.getElementById(seasonId).addEventListener('click', ()=>eventWrapper(event,()=>toggleFavorites(event)), false);
 }
 
 function toggleFavorites(){
     let season = event.target.getAttribute('data-season'),
-        race = event.target.getAttribute('data-race');
-        
-    if(localStorage.getItem("favoritesExist") === null){
-        let tmpObj = {};
-        
-        localStorage.setObj("favorites", tmpObj);
-        localStorage.setItem("favoritesExist","yes");
-    } 
-    let favorites = localStorage.getObj("favorites");
-    if(!favorites.hasOwnProperty(season)){
-        favorites[season] = [race];
-        localStorage.setObj("favorites", favorites);
+        race = event.target.getAttribute('data-race'),
+        favorites = localStorage.getObj("favorites");
+        favoritesButton = "-";
+    
+    if(checkFavorites(season, race)){
+        favorites[season].splice(favorites[season].indexOf(race), 1);
+        if(favorites[season].length == 0) delete favorites[season];
+        favoritesButton = "+";
     }else{
-        if(favorites[season].includes(race)){
-            favorites[season].splice(favorites[season].indexOf(race), 1);
-            if(favorites[season].length == 0) delete favorites[season];
-        }else{
-            favorites[season].push(race);
-        }
+        if(!favorites.hasOwnProperty(season)) favorites[season] = [];
+        favorites[season].push(race);
     }
     localStorage.setObj("favorites", favorites);
-    console.log(localStorage.getObj("favorites"));
+    event.target.innerHTML = favoritesButton;
 }
 
 function checkFavorites(season, race){
@@ -121,19 +100,54 @@ function checkFavorites(season, race){
     } 
     let favorites = localStorage.getObj("favorites");
     if(favorites.hasOwnProperty(season)){
-        if(favorites[season].includes(race)){
+        if(favorites[season].indexOf(race)!==-1){
             return true;
         }
     }
     return false;
 }
 
+function renderFavorites(){
+    let favorites = localStorage.getObj("favorites"),
+        html = "<table>";
+    Object.keys(favorites).forEach(season=> {
+        let races = CACHE.seasons[season];
+        favorites[season].forEach(race=>{
+            races.filter(item => {
+                if(item.round === race) html += seasonListTemplate(item);
+              });
+            
+        });
+    });
+    
+    html += "</table>";
+    document.getElementById("racesFavorites").innerHTML = html;
+}
+
+function seasonListTemplate(race){
+    let favoritesButton = (checkFavorites(race.season, race.round)) ? "-" : "+";
+    html = `
+                <tr>
+                    <td>${race.round}</td>
+                    <td>${race.raceName}</td>
+                    <td>${race.date}</td>
+                    <td>${race.Circuit.circuitName}</td>
+                    <td>${race.Circuit.Location.locality}, ${race.Circuit.Location.country}</td>
+                    <td>${race.Results[0].Driver.givenName} ${race.Results[0].Driver.familyName}</td>
+                    <td>${race.Results[0].Constructor.name}</td>
+                    <td><button class="add-fav" data-season="${race.season}" data-race="${race.round}">${favoritesButton}</button></td>
+                </tr>`;
+    return html;
+}
+
 window.onload = () => {
     buildSeasonsList();
+    localStorage.clear();
     Storage.prototype.setObj = function(key, obj) {
         return this.setItem(key, JSON.stringify(obj))
     }
     Storage.prototype.getObj = function(key) {
         return JSON.parse(this.getItem(key))
     }
+    document.getElementById("showFavorites").addEventListener('click', renderFavorites);
 };
